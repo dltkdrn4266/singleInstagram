796501a2032a91f50886f18369e3c9f11e67b4a7
Sang gu Lee <dltkdrn4266@naver.com>
2019-02-12 14:39:59 +0900

    [add] get_post_list method

--- instagram/views.py
+++ instagram/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .serializers import PostSerializer
 from .models import Post
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 class PostView(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializers_class = PostSerializer
-    permission_classes   = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAuthenticated,)
 
     def perform_create(self, serializers):
         serializers.save(user=self.request.user)
@@ -14,3 +16,15 @@ class PostView(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.request.user.is_staff:
             return PostSerializer
+
+    @api_view(['GET', 'POST'])
+    def get_post_list(request):
+        if request.method == 'GET':
+            serializers = PostSerializer(queryset, many=True)
+            return Response(serializers.data)
+        elif request.method == 'POST':
+            serializers = PostSerializer(data=request.data)
+            if serializers.is_vaild():
+                serializers.save()
+                return Response(serializers.data, status=status.HTTP_201_CREATED)
+            return Response(serializers.error, status=status.HTTP_400_BAD_REQUEST)
