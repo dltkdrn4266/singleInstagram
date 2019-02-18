1bb6e6c3c0dcbfe79df184917b063ed5cba01261
Sang gu Lee <dltkdrn4266@naver.com>
2019-02-18 16:50:36 +0900

    [Add] Post model default add

--- instagram/models.py
+++ instagram/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    photos = models.ImageField()
+    photos = models.ImageField(default='media/default.jpg')
     content = models.TextField(blank=True)
-    like = models.BooleanField()
+    like = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
