ca41d2fe92692c2fa2b114fcdcd492c4be76ceba
sanggulee <dltkdrn4266@naver.com>
2019-04-08 15:58:13 +0900

    [change] DecimalField -> FloatField

--- instagram/models.py
+++ instagram/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.gis.geos import Point
-from django.db.models import Manager as GeoManager
 
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     photos = models.ImageField(default='default.jpg')
-    latitude = models.DecimalField(max_digits=10,decimal_places=10)
-    longitude = models.DecimalField(max_digits=10,decimal_places=10)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
     content = models.TextField(blank=True)
     like = models.BooleanField(default=False,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
