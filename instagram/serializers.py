b3e0c16025f87d4a62c2486ce70a747af3c0f89f
sanggulee <dltkdrn4266@naver.com>
2019-05-16 17:04:41 +0900

    [add] created_at read_only true

--- instagram/serializers.py
+++ instagram/serializers.py
@@ -47,7 +47,7 @@ class PostSerializer(serializers.HyperlinkedModelSerializer):
     photos = Base64ImageField(
         max_length=None, use_url=True,
     )
-    created_at = serializers.DateTimeField(format="%Y-%m-%d")
+    created_at = serializers.DateTimeField(format="%Y-%m-%d",read_only=True)
     class Meta:
         model = Post
         fields = (
